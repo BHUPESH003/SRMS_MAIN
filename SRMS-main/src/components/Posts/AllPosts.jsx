dac00279b0d9850a4ca4a5a3d6fdc57fadc1bb4a
Bhupesh <93593639+BHUPESH003@users.noreply.github.com>
2023-10-17 19:49:40 +0530

    fixed some minor bugs

--- SRMS-main/src/components/Posts/AllPosts.jsx
+++ SRMS-main/src/components/Posts/AllPosts.jsx
@@ -13,7 +13,7 @@ export default function AllPosts() {
   return (
     <div className="container mx-auto p-6">
       <h1 className="text-3xl font-semibold mb-6">Latest Posts</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2  gap-4">
         {posts.length > 0 ? (
           posts.map(post => (
             <Post key={post._id} {...post} />
